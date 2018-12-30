4d841676623df8acd61adae957e489a826490920
DESKTOP-PU1GBV0\etred <etredal@gmail.com>
2018-12-30 15:08:20 -0600

    Base Project

---
+++ Assets/ProjectInformation.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * What is this game?
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Things to include:
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * Release platforms:
+ * -Android
+ * -IOS
+ * -Maybe Windows (since it is so easy to do)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * /
