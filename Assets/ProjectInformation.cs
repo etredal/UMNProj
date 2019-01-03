a7dc38d57f68388a8a77d4153b9ca14e117db014
paulinglucas <paulinglucas@gmail.com>
2019-01-03 15:00:06 -0600

    updated game info file

--- Assets/ProjectInformation.cs
+++ Assets/ProjectInformation.cs
@@ -1,9 +1,9 @@
 ﻿/*
  * What is this game?
+ * Copy of an "endless mode" version of Zombie Trailer Park
+ * Dogs protect their doghouse from a neverending swarm of cats
  * 
- * 
- * 
- * 
+ * Different types of dogs to deploy against different cat enemies
  * 
  * 
  * 
@@ -13,11 +13,14 @@
  * 
  * Things to include:
  * 
+ * Dog, cat class with different types of dogs and cats to inherit from the super class
  * 
+ * Powerups that players can use every so often to get rid of swarms
  * 
+ * Buildings, including one that makes money for the player to use to build other buildings
+ * One building that can increase amount of dogs deployed at a time
  * 
- * 
- * 
+ * Score counter that tallies up the more cats that are demolished
  * 
  * 
  * Release platforms:
@@ -32,4 +35,4 @@
  * 
  * 
  * 
- * /
+ */
