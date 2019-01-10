d957edd164460b3f03c95bbd27cd8f540244b178
DESKTOP-CNI4KC5\etred <etredal@gmail.com>
2019-01-09 20:40:41 -0500

    Corgi Movement
    Added corgi sprite and script so it can move across the screen.

---
+++ Assets/CorgiBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorgiBehavior : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 movement = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += movement;   
+    }
+}
